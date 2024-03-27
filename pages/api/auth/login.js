fbc628eadc106499999b4bc4a60c278ae8307b8b
David Weaver <davidweav322@gmail.com>
2024-03-27 17:13:57 -0400

    connects frontend login to api

--- pages/api/auth/login.js
+++ pages/api/auth/login.js
@@ -1,22 +1,5 @@
-import { UserModel } from '../../../models';
-import bcrypt from 'bcrypt';
 
-export async function signIn(usernameOrEmail, password) {
-  // Check if usernameOrEmail is an email or username
-  const query = usernameOrEmail.includes('@') ? { email: usernameOrEmail } : { username: usernameOrEmail };
 
-  // Find the user by email or username
-  const user = await UserModel.findOne(query);
-  if (!user) {
-    throw new Error('User not found.');
-  }
-
-  // Compare passwords
-  const passwordMatch = await bcrypt.compare(password, user.password);
-  if (!passwordMatch) {
-    throw new Error('Incorrect password.');
-  }
-
-  // Return the authenticated user
-  return user;
+export default async function handler(req, res) {
+  res.status(200).json({ message: 'api route works' });
 }
--- pages/api/auth/register.js
+++ pages/api/auth/register.js
@@ -1,19 +1,5 @@
-import UserModel from '../models';
 
-export async function register(req, res) {
-  // Validate email format
-  console.log('Email:', email);
 
-  // Check if the user already exists
-  const existingUser = await UserModel.findOne({ email });
-  if (existingUser) {
-    throw new Error('User already exists.');
-  }
-
-  // Hash the password
-  const hashedPassword = await hashPassword(password);
-
-  // Create a new user document
-  const newUser = new UserModel({ username, email, password: hashedPassword });
-  await newUser.save();
+export default async function handler(req, res) {
+  res.status(200).json({ message: 'api route works' });
 }
