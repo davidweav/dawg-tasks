930ca3383376b89e05a9585c6575cd220572b1ef
David Weaver <davidweav322@gmail.com>
2024-04-10 18:34:36 -0400

    updates readposts

--- pages/api/posts/readposts.js
+++ pages/api/posts/readposts.js
@@ -1,25 +1,9 @@
+import PostModel from "@/models/post";
 
 
 export default async function handler(req, res) {
 
-    res.status(200).json({posts:[
-        {
-            taskId: 200,
-            publisher: "davidweav",
-            description: "Help me walk my dog",
-            dueDate: "Friday April 12",
-            reward: 10,
-            status: "Unclaimed"
-        },
-        {
-            taskId: 200,
-            publisher: "adamruehle",
-            description: "Wipe my ass",
-            dueDate: "Thursday April 11",
-            reward: 100,
-            status: "Unclaimed"
-        }]
-        
-    })
-    
+    const posts = await PostModel.find({});
+    res.status(200).json({posts: posts });
+
 }
